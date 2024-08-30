05e86b4d6739e66de618b32399502fbd5734512f
Jonas Rapp <jonas@jonasr.app>
2024-08-30 10:56:58 +0200

    Added some comments to explain the dumb plugin

--- Plugin/Canary.cs
+++ Plugin/Canary.cs
@@ -24,18 +24,23 @@ namespace Rappen.Canary365.Plugin
                 {
                     ts.Trace("Configuration:\n{0}", _unsec);
                 }
+
+                // Classic initialization for plugin context - should be in a Base Class...
                 var ctx = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
                 var sfact = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                 var svc = sfact.CreateOrganizationService(ctx.UserId);
 
+                // Flags that may ge set in the unsecure configuration
                 var parentcontext = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("PARENTCONTEXT=TRUE");
                 var attributetypes = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("ATTRIBUTETYPES=TRUE");
                 var convertqueries = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("CONVERTQUERIES=TRUE");
                 var expandcollections = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("EXPANDCOLLECTIONS=TRUE");
                 var includestage30 = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("INCLUDESTAGE30=TRUE");
 
+                // Calling CanaryTracer!
                 ts.TraceContext(ctx, parentcontext, attributetypes, convertqueries, expandcollections, includestage30, svc);
 
+                // Trying to use the ILogger service
                 var pt = (ILogger)serviceProvider.GetService(typeof(ILogger));
                 try
                 {
