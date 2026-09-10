fe72f93d3cfc3b56722419c5dc51cabe443d62eb
Jonas Rapp <jonas@jonasr.app>
2026-09-10 17:20:50 +0200

    Using even simpler calls

--- Plugin/Canary.cs
+++ Plugin/Canary.cs
@@ -16,52 +16,56 @@ namespace Rappen.Canary365.Plugin
 
         public void Execute(IServiceProvider serviceProvider)
         {
-            var ts = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-            try
+            serviceProvider.TraceContext();
+
+            if (serviceProvider.GetService(typeof(ITracingService)) is ITracingService ts)
             {
-                ts.Trace("Trace enter: {0:o}\n", DateTime.Now);
-                if (!string.IsNullOrEmpty(_unsec))
-                {
-                    ts.Trace("Configuration:\n{0}", _unsec);
-                }
-
-                // Classic initialization for plugin context - should be in a Base Class...
-                var ctx = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-                var sfact = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-                var svc = sfact.CreateOrganizationService(ctx.UserId);
-
-                // Flags that may ge set in the unsecure configuration
-                var parentcontext = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("PARENTCONTEXT=TRUE");
-                var attributetypes = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("ATTRIBUTETYPES=TRUE");
-                var convertqueries = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("CONVERTQUERIES=TRUE");
-                var expandcollections = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("EXPANDCOLLECTIONS=TRUE");
-                var includestage30 = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("INCLUDESTAGE30=TRUE");
-
-                // Calling CanaryTracer!
-                ts.TraceContext(ctx, parentcontext, attributetypes, convertqueries, expandcollections, includestage30, svc);
-
-                // Trying to use the ILogger service
-                var pt = (ILogger)serviceProvider.GetService(typeof(ILogger));
                 try
                 {
-                    pt.LogInformation("Canary write by LogInformation");
-                    pt.LogTrace("Canary write by LogTrace");
-                    ts.Trace("PluginTelemetry OK");
+                    ts.Trace("Trace enter: {0:o}\n", DateTime.Now);
+                    if (!string.IsNullOrEmpty(_unsec))
+                    {
+                        ts.Trace("Configuration:\n{0}", _unsec);
+                    }
+
+                    // Classic initialization for plugin context - should be in a Base Class...
+                    var ctx = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                    var sfact = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    var svc = sfact.CreateOrganizationService(ctx.UserId);
+
+                    // Flags that may ge set in the unsecure configuration
+                    var parentcontext = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("PARENTCONTEXT=TRUE");
+                    var attributetypes = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("ATTRIBUTETYPES=TRUE");
+                    var convertqueries = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("CONVERTQUERIES=TRUE");
+                    var expandcollections = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("EXPANDCOLLECTIONS=TRUE");
+                    var includestage30 = !string.IsNullOrEmpty(_unsec) && _unsec.ToUpperInvariant().Contains("INCLUDESTAGE30=TRUE");
+
+                    // Calling CanaryTracer!
+                    ts.TraceContext(ctx, parentcontext, attributetypes, convertqueries, expandcollections, includestage30, svc);
+
+                    // Trying to use the ILogger service
+                    var pt = (ILogger)serviceProvider.GetService(typeof(ILogger));
+                    try
+                    {
+                        pt.LogInformation("Canary write by LogInformation");
+                        pt.LogTrace("Canary write by LogTrace");
+                        ts.Trace("PluginTelemetry OK");
+                    }
+                    catch (Exception ex)
+                    {
+                        ts.Trace("PluginTelemetry Error:");
+                        ts.Trace(ex.Message);
+                        pt.LogError(ex, ex.Message);
+                    }
                 }
                 catch (Exception ex)
                 {
-                    ts.Trace("PluginTelemetry Error:");
-                    ts.Trace(ex.Message);
-                    pt.LogError(ex, ex.Message);
+                    ts.Trace("††† Canary is dead †††\n{0}", ex);
                 }
-            }
-            catch (Exception ex)
-            {
-                ts.Trace("††† Canary is dead †††\n{0}", ex);
-            }
-            finally
-            {
-                ts.Trace("\nTrace exit: {0:o}\n", DateTime.Now);
+                finally
+                {
+                    ts.Trace("\nTrace exit: {0:o}\n", DateTime.Now);
+                }
             }
         }
     }
